d43bc627dae24103190c1a6ad4dd689b9564b889
baonekmn <quocbaongo3@gmail.com>
2026-06-12 07:12:53 +0700

    bug ngoo

---
+++ gallery.js
@@ -0,0 +1,13 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic) {
+    let imageDiv = document.getElementById("image");
+    imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
+    imageDiv.innerHTML = previewPic.alt;
+}
+
+function unDo() {
+    let imageDiv = document.getElementById("image");
+    imageDiv.style.backgroundImage = "url('')";
+    imageDiv.innerHTML = "Hover over an image below to display here.";
+}
